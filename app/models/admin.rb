361a699a3ce9b0f8f3aae299d3478a8d4171c9d3
Benjamin Peña Olvera <benjamin.pena.olvera@gmail.com>
2018-04-02 23:42:16 -0500

    rubocop offences

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,9 +2,7 @@
 
 class Admin < ApplicationRecord
   has_many :posts
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
 end
